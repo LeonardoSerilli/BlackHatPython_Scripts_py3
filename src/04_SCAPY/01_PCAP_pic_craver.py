cd2aca154cbc4fb86dd885277d5d171fc311049d
LeonardoSer <leoser14592@gmail.com>
2023-10-10 14:49:46 +0200

    Scapy Arpoisoning and Pic craver

--- src/04_SCAPY/01_PCAP_pic_craver.py
+++ src/04_SCAPY/01_PCAP_pic_craver.py
@@ -2,12 +2,37 @@ import os
 import re
 from scapy.all import TCP, rdpcap
 import zlib # zlib is commonly used for compression and decompression 
+import cv2 # OpenCV (Open Source Computer Vision Library), which is a popular open-source computer vision and machine learning software library
+
+
+"""
+## PREREQUISITES
+[*] You just need a .pcap with some images exchanged via HTTP
+    [=>] To obtain you own .pcap to try the code do the following:
+        - open wirehark on loopback interface
+        - open a listener with " nc -lvnp 80"
+        - download an image with faces in it 
+        - send the image to yourself through HTTP 
+            "curl -X POST -H "Content-Type: image/jpeg" --data-binary "@img.jpg" http://127.0.0.1:80 
+        - stop wireshark and save the pcap: "file->save as->.pcap"
+"""
 
 PIC_DIR = "src/04_SCAPY/01_PCAPics"
-PCAP_PATH = "src/04_SCAPY/capture.pcap"
-#PCAP_PATH = "/home/rorschach/Downloads/http.pcap"
+PCAP_PATH = "src/04_SCAPY/01_capture.pcap"
 
 def http_assembler():
+    """
+    Assembles HTTP content from a PCAP file and extracts images.
+
+    Reads in a PCAP file and reconstructs TCP streams. Checks for HTTP requests
+    and extracts images from responses if found. Stores images and attempts 
+    face detection.
+
+    Returns:
+    carved_images (int): Number of images extracted from PCAP.
+    faces_detected (int): Number of faces detected in extracted images.
+    """
+
     carved_images = 0
     faces_detected = 0
 
@@ -49,17 +74,32 @@ def http_assembler():
 
             carved_images += 1
 
-            # # Attempt face detection
-            # try:
-            #     result = face_detect(f"{PIC_DIR}/{file_name}", file_name)
-            #     if result is True:
-            #         faces_detected += 1
-            # except:
-            #     pass
+            # Attempt face detection
+            try:
+                result = face_detect(f"{PIC_DIR}/{file_name}", file_name, faces_detected)
+                if result is True:
+                    faces_detected += 1
+            except:
+                pass
 
     return carved_images, faces_detected
     
 def extract_image(headers, http_payload):
+    """Extracts image data from HTTP headers and payload.
+
+    Checks for image content type in headers. Finds start of image 
+    data based on header-body separator. Decompresses image if compressed.
+
+    Args:
+    headers (dict): HTTP headers
+    http_payload (bytes): HTTP payload
+
+    Returns:
+    tuple:
+    image (bytes): Extracted image bytes 
+    image_type (str): Image MIME type
+    """
+
     # Initialize variables to None
     image = None
     image_type = None
@@ -108,9 +148,19 @@ def extract_image(headers, http_payload):
     # Return the extracted image data and image type
     return image, image_type
 
-import re
 
 def get_http_headers(http_payload):
+    """Extracts HTTP headers from a HTTP payload.
+
+    Finds the header-body separator in the payload. Decodes the header portion and converts to a dictionary.
+
+    Args:
+    http_payload (bytes): The HTTP payload
+
+    Returns:
+    dict: The extracted HTTP headers  
+    """
+
     headers = None
     try:
         if http_payload:
@@ -125,37 +175,58 @@ def get_http_headers(http_payload):
     except:
         pass
     
-    return None  # Return None if headers extraction failed
-
-# ...
+    return headers  # Return None if headers extraction failed
 
 
-# def face_detect(path, file_name):
-#     """
-#     Detect faces in an image using OpenCV and save the image with detected faces highlighted.
+def face_detect(path, file_name, faces_detected):
+    """Performs face detection on an image.
 
-#     :param path: Path to the image.
-#     :param file_name: Name of the image file.
-#     :return: True if faces are detected, False otherwise.
-#     """
-#     img = cv2.imread(path)
-#     cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
-#     rects = cascade.detectMultiScale(img, 1.3, 4, cv2.CASCADE_SCALE_IMAGE, (20, 20))
+    Uses OpenCV's Haar Cascade classifier to detect faces. 
+    Draws rectangles around detected faces. Saves the image with faces highlighted.
+
+    Args:
+    path (str): Path to the image file
+    file_name (str): Name of the image file
+    faces_detected (int): Current number of faces detected
     
-#     if len(rects) == 0:
-#         return False
+    Returns:
+    bool: True if faces were detected, False otherwise
+    """
 
-#     rects[:, 2:] += rects[:, :2]
+    img = cv2.imread(path)
 
-#     # Highlight the faces in the image
-#     for x1, y1, x2, y2 in rects:
-#         cv2.rectangle(img, (x1, y1), (x2, y2), (127, 255, 0), 2)
+    # Use the default Haar Cascade classifier for face detection
+    cascade = cv2.CascadeClassifier(
+        f'{cv2.data.haarcascades}haarcascade_frontalface_default.xml'
+    )    
+        
+    rects = cascade.detectMultiScale(img, 1.3, 4, cv2.CASCADE_SCALE_IMAGE, (20, 20))
 
-#     cv2.imwrite("%s/%s-%s" % (faces_directory, pcap_file, file_name), img)
-#     return True
+    if len(rects) == 0:
+        return False
+
+    rects[:, 2:] += rects[:, :2]
+
+    # Highlight the faces in the image
+    for x1, y1, x2, y2 in rects:
+        cv2.rectangle(img, (x1, y1), (x2, y2), (127, 255, 0), 2)
+
+    # Construct the full path for saving the image
+    
+    # Split the file_name into name and extension
+    name, ext = os.path.splitext(file_name)    
+    save_path = f"{PIC_DIR}/{name}_face_{str(faces_detected) + ext}"
+    
+    print(f"[*] saving image at {save_path}")
+    cv2.imwrite(save_path, img)
+    
+    return True
 
 
 def main():
+    """
+    Calls the http_assembler function to extract images from a PCAP file.
+    """
     # Main execution
     carved_images, faces_detected = http_assembler()
     print("Extracted: %d images" % carved_images)
